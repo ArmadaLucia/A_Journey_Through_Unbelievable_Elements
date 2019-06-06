594bbfb3893cc403bbe5ba102c39504cc6d4b17f
Vanessa van den Akker <vanessavandenakker@gmail.com>
2019-06-06 16:25:12 +0200

    Ja beeetje dingen

---
+++ DuckItOn/Assets/scripts/DuckFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DuckFollow : MonoBehaviour
+{
+
+    public Transform player;
+    // Vector3 stores 3 floats
+    // offset will give us oppurtunity to change camera position (not first person view)
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // camera will only follow the position of the player (not rotate with him)
+        // camera transform("movement", scale, x, y, z) will follow the player position
+        // + offset, because then it will still follow the player on the new position
+        transform.position = player.position + offset;
+    }
+}
---
+++ DuckItOn/Assets/scripts/DuckMove.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class DuckMove : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce;
+    public float rotation;
+    public float DuckForce;
+
+    private bool canPressSpace = true;
+
+    public int maxtime;
+    private int currenttime;
+
+    //half 3 karin volgende week donderdag
+    // velocity is een richting + een snelheid
+
+    void FixedUpdate()
+    {
+        //if (rb.velocity.sqrMagnitude <= forwardForce * forwardForce)
+        //{
+        //    
+        //}
+        //else
+        //{
+        //    canPressSpace = true;
+        //}
+
+        rb.AddForce(0, 0, forwardForce);
+
+        if (currenttime <= 0)
+        {
+            canPressSpace = true;
+        }
+
+        else
+        {
+            currenttime -= 1;
+        }
+
+        //Debug.Log(rb.velocity.magnitude);
+
+        int left = Input.GetKey("a") ? 1 : 0;
+        int right = Input.GetKey("d") ? 1 : 0;
+        int turn = -left + right;
+        Debug.Log(turn);
+        rb.AddTorque(transform.up * rotation * turn);
+
+
+        //if (Input.GetKey("d"))
+        //{
+        //    transform.Rotate(new Vector3(0, rotation, 0));
+        //}
+        //if (Input.GetKey("a"))
+        //{
+        //    transform.Rotate(new Vector3(0, -rotation, 0));
+        //}
+        if (Input.GetKeyDown("space") && canPressSpace)
+        {
+            //transform.Translate(0, 0, DuckForce);
+            Debug.Log("hi");
+            rb.velocity = Vector3.zero;
+            rb.AddForce(transform.forward * DuckForce); //ForceMode.VelocityChange);
+            canPressSpace = false;
+            currenttime = maxtime;
+        }
+    }
+}
